82fafa7650afd87d40899685945ad298c332afa1
ripoll97 <miquel97ripoll@gmail.com>
2018-03-07 18:08:28 +0100

    Ball movement initial script

---
+++ Assets/Scripts/PongBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongBall : MonoBehaviour {
+
+    public Rigidbody2D pongBall;
+
+	// Use this for initialization
+	void Start () {
+        pongBall = this.GetComponent<Rigidbody2D>();
+        int rand = Random.Range(0, 2);
+        if (rand == 0)
+        {
+           pongBall.velocity = new Vector2(-2f, 0f);
+        }
+        else if (rand == 1)
+        {
+            pongBall.velocity = new Vector2(2f, 0f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
