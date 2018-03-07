6254afa39d7f0dc33c64f7f4e721055aabc7fefb
Miquel Postigo Llabres <miquelpostigo@gmail.com>
2018-03-07 17:27:10 +0100

    Initial Commit

---
+++ Assets/Scripts/Jugador1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador1 : MonoBehaviour {
+
+    public GameObject pala;
+    // Use this for initialization
+    Vector3 moveup = new Vector3(0, 0.1f, 0);
+
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            pala.transform.Translate(moveup);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            pala.transform.Translate(-moveup);
+        }
+    }
+}
