3ddd6b2affc3e97e30410a129d9b3363d0fc9160
ripoll97 <miquel97ripoll@gmail.com>
2018-03-13 20:25:37 +0100

    All in 2D

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour {
 
-    private Rigidbody RB;
+    private Rigidbody2D RB;
     private float ballVelocity = 2f;
     private ScoreController scoreController;
     private float hitOffset = 10f;
 
     // Use this for initialization
     void Start () {
-        RB = GetComponent<Rigidbody>();
+        RB = GetComponent<Rigidbody2D>();
 
         int xDirection = Random.Range(0, 2);
         float yDirection = Random.value;
@@ -31,28 +31,35 @@ public class BallController : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "BoundariesTop"){
-            RB.velocity = new Vector3((RB.velocity.normalized).x * ballVelocity, (RB.velocity.normalized).y * ballVelocity * -1f, 0f);
+       if (collision.gameObject.tag == "Boundaries"){
+            RB.velocity = new Vector2(RB.velocity.x, RB.velocity.y);
+            //RB.velocity = new Vector2((RB.velocity.normalized).x * ballVelocity, -((RB.velocity.normalized).y * ballVelocity));
+            Debug.Log("Fuck");
         }
-        if (collision.gameObject.tag == "BoundariesBottom")
+        /*if (collision.gameObject.tag == "BoundariesBottom")
         {
             RB.velocity = new Vector3((RB.velocity.normalized).x * ballVelocity, (RB.velocity.normalized).y * ballVelocity * -1f, 0f);
-        }
+        }*/
 
         if (collision.gameObject.tag == "PlayerLeft"){
             float dist = this.transform.position.y - GameObject.Find("PlayerLeft").transform.position.y;
-            RB.velocity = new Vector3(ballVelocity, dist * hitOffset, 0f);
+            /*if (collision.contacts.Length > 0) {
+                Vector2 variation = collision.contacts[0].point - collision.gameObject.transform.position;
+                variation.Normalize();
+            }*/
+            RB.velocity = new Vector2(ballVelocity, dist * hitOffset);
+            
         }
         if (collision.gameObject.tag == "PlayerRight")
         {
             float dist = this.transform.position.y - GameObject.Find("PlayerRight").transform.position.y;
-            RB.velocity = new Vector3(-ballVelocity, dist * hitOffset, 0f);
+            RB.velocity = new Vector2(-ballVelocity, dist * hitOffset);
         }
         Debug.Log(collision.gameObject.tag);
-        if (collision.gameObject.tag == "Brick")
+        /*if (collision.gameObject.tag == "Brick")
         {
             RB.velocity = new Vector3((RB.velocity.normalized).x * ballVelocity * -1f, (RB.velocity.normalized).y * ballVelocity, 0f);
-        }
+        }*/
 
         if (collision.gameObject.tag == "GoalLeft"){ scoreController.leftScore++; }
         if (collision.gameObject.tag == "GoalRight"){ scoreController.rightScore++; }
